6209ab108df4f18d1531bfe3c9e7d5b56bf16a32
dvogelg4al <danny.hj.vogel@gmail.com>
2024-02-11 21:33:39 +0100

    Feat: add link to home

--- components/Header.vue
+++ components/Header.vue
@@ -4,8 +4,10 @@
   <div
     class="flex justify-center items-start h-40 border border-slate-300 p-5 rounded"
   >
-    <h1 class="font-satisfy italic text-[72px] text-center text-cyan-700">
-      musigh
-    </h1>
+    <NuxtLink to="/">
+      <h1 class="font-satisfy italic text-[72px] text-center text-cyan-700">
+        musigh
+      </h1>
+    </NuxtLink>
   </div>
 </template>
