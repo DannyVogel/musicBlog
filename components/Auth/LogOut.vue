1ee39031f60af8885599c06269c83cda8b90ba19
dvogelg4al <danny.hj.vogel@gmail.com>
2024-02-11 21:33:51 +0100

    Feat: init logout button

--- components/Auth/LogOut.vue
+++ components/Auth/LogOut.vue
@@ -3,5 +3,10 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <UButton color="red" label="Sign Out" @click="authStore.logOut" />
+  <UButton
+    color="red"
+    label="Sign Out"
+    variant="outline"
+    @click="authStore.logOut"
+  />
 </template>
