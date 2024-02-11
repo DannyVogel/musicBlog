3ba85c9583be71573dcc6e38c63a39eb418d2c8a
dvogelg4al <danny.hj.vogel@gmail.com>
2024-02-11 21:40:01 +0100

    Feat: init adin page

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,11 +1,23 @@
 <script lang="ts" setup>
 const authStore = useAuthStore();
+
+const postsStore = usePostsStore();
+onMounted(async () => {
+  await postsStore.getPosts();
+});
 </script>
 
 <template>
   <div class="mt-4">
     <AuthAccess v-if="!authStore.isLoggedIn" />
-    <div v-else>
+    <div v-else class="flex flex-col items-center gap-8">
+      <h1 class="text-2xl text-cyan-700 text-center">
+        Welcome {{ authStore.userName }}
+      </h1>
+      <div class="text-black">
+        <p>Total Posts: {{ postsStore.getPostsCount }}</p>
+        <p>Total Likes: {{ postsStore.getTotalLikes }}</p>
+      </div>
       <PostCreator />
       <AuthLogOut />
     </div>
